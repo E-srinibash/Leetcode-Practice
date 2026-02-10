4f5d549f4ca2c406b08514371165813265ac5c4b
E Srinibash <ellasrinibash0@gmail.com>
2026-02-11 00:00:08 +0530

    modified code for ContainerWithMostWater

--- src/two-pointers/ContainerWithMostWater11.java
+++ src/two-pointers/ContainerWithMostWater11.java
@@ -1,26 +1,29 @@
-public static int maxArea(int[] height) {
+public class ContainerWithMostWater11{
+    public static int maxArea(int[] height) {
         int n = height.length;
         int maxWater = 0;
         int left = 0;
         int right = n-1;
+        
         while(left<right){
-        int currHeight = Math.min(height[left],height[right]);
+            int currHeight = Math.min(height[left],height[right]);
 
-        int currentArea = currHeight * (right - left);
+            int currentArea = currHeight * (right - left);
 
-        maxWater = Math.max(currentArea,maxWater);
+            maxWater = Math.max(currentArea,maxWater);
 
-        if(height[left] <= height[right]){
-            left++;
-        }
-        else{
-            right--;
-        }
+            if(height[left] <= height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
         }
         return maxWater;
     }
 
-public static void main (String args[]){
-  int[] heights = new int[]{1,8,6,2,5,4,8,3,7};
-  System.out.println(maxArea(heights));
+    public static void main (String args[]){
+        int[] heights = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(heights));
+    }
 }
